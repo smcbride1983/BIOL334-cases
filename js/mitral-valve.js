8fdfe90e557a4ff088d77cbd2626a2f4678a5321
smcbride1983 <mcbridsg@vt.edu>
2026-06-06 00:07:37 -0400

    Refactor diagnosis and test results structure

--- js/mitral-valve.js
+++ js/mitral-valve.js
@@ -1,26 +1,10 @@
 const correctDiagnosis =
-"Viridans streptococcus";
+    "Viridans streptococcus";
+
+const acceptedDiagnoses = [
+    ...
+];
 
 const testResults = {
-
- gram: {
-   image:
-   "../images/gram/gram_positive_cocci_chains.jpg"
- },
-
- catalase: {
-   image:
-   "../images/catalase/catalase_negative.jpg"
- },
-
- bloodAgar: {
-   image:
-   "../images/blood_agar/alpha_hemolysis.jpg"
- },
-
- kirby: {
-   image:
-   "../images/kirby/viridans_strep.jpg"
- }
-
+    ...
 };
