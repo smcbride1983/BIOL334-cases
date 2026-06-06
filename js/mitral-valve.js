da2dc32f9d7aac3083006f891f06cfbaa3d847b3
smcbride1983 <mcbridsg@vt.edu>
2026-06-05 23:47:20 -0400

    Add test results for Viridans streptococcus diagnosis

--- js/mitral-valve.js
+++ js/mitral-valve.js
@@ -1,1 +1,26 @@
+const correctDiagnosis =
+"Viridans streptococcus";
 
+const testResults = {
+
+ gram: {
+   image:
+   "../images/gram/gram_positive_cocci_chains.jpg"
+ },
+
+ catalase: {
+   image:
+   "../images/catalase/catalase_negative.jpg"
+ },
+
+ bloodAgar: {
+   image:
+   "../images/blood_agar/alpha_hemolysis.jpg"
+ },
+
+ kirby: {
+   image:
+   "../images/kirby/viridans_strep.jpg"
+ }
+
+};
