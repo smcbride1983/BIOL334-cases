536bc3e5c33f56e30c8b80393ee771ab78dec6d5
smcbride1983 <mcbridsg@vt.edu>
2026-06-05 23:46:25 -0400

    Add test results and diagnosis for Staphylococcus epidermidis

--- js/prosthetic-aortic-valve.js
+++ js/prosthetic-aortic-valve.js
@@ -1,1 +1,36 @@
-prosthetic-aortic-valve
+const correctDiagnosis =
+"Staphylococcus epidermidis";
+
+const testResults = {
+
+ gram: {
+   image:
+   "../images/gram/gram_positive_cocci_clusters.jpg"
+ },
+
+ catalase: {
+   image:
+   "../images/catalase/catalase_positive.jpg"
+ },
+
+ coagulase: {
+   image:
+   "../images/coagulase/coagulase_negative.jpg"
+ },
+
+ bloodAgar: {
+   image:
+   "../images/blood_agar/nonhemolytic_white_colonies.jpg"
+ },
+
+ macconkey: {
+   image:
+   "../images/macconkey/no_growth.jpg"
+ },
+
+ kirby: {
+   image:
+   "../images/kirby/staph_epidermidis.jpg"
+ }
+
+};
