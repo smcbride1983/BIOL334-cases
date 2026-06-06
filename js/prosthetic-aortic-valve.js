f0bdb3678bab46f3e57164b67e1b12b0bac19439
smcbride1983 <mcbridsg@vt.edu>
2026-06-06 00:07:20 -0400

    Update diagnosis and restructure test results
    Refactor diagnosis and test results structure.

--- js/prosthetic-aortic-valve.js
+++ js/prosthetic-aortic-valve.js
@@ -1,36 +1,12 @@
 const correctDiagnosis =
-"Staphylococcus epidermidis";
+    "Staphylococcus epidermidis";
+
+const acceptedDiagnoses = [
+    "staphylococcus epidermidis",
+    "s epidermidis",
+    "staph epidermidis"
+];
 
 const testResults = {
-
- gram: {
-   image:
-   "../images/gram/gram_positive_cocci_clusters.jpg"
- },
-
- catalase: {
-   image:
-   "../images/catalase/catalase_positive.jpg"
- },
-
- coagulase: {
-   image:
-   "../images/coagulase/coagulase_negative.jpg"
- },
-
- bloodAgar: {
-   image:
-   "../images/blood_agar/nonhemolytic_white_colonies.jpg"
- },
-
- macconkey: {
-   image:
-   "../images/macconkey/no_growth.jpg"
- },
-
- kirby: {
-   image:
-   "../images/kirby/staph_epidermidis.jpg"
- }
-
+    ...
 };
